f6f926f7e8cddca583f6c440eb1a94b1003b2276
youchen <you_chen183@163.com>
2022-04-17 21:15:47 +0800

    feat: test

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -114,7 +114,7 @@ export default {
 		console.log('mounted hello')
 	},
 	destroyed() {
-		console.log('destoryed hello')
+		console.log('destoryed helloq')
 	},
 	beforeDestroy() {
 		console.log('before destory hello')
