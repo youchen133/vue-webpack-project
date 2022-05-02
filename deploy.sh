052b32a80ac6faecfd2af007eb7ad348c733b1de
youchen <you_chen183@163.com>
2022-05-02 13:10:41 +0800

    feat: 调整打包部署

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:youchen133/vue-webpack-project.git master:gh-pages
+git push -f git@github.com:youchen133/vue-webpack-project.git branch0.1:gh-pages
 
 cd -
